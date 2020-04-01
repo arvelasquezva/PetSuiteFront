5e0f80b9e425df15f091ec358b86468217bda27f
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-03-31 21:53:21 -0500

    Create NavBarComponent

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="app">
+    <b-navbar toggleable="lg" type="dark" class="navbarPetSuite">
+      <b-container>
+        <b-navbar-brand href="/">
+          <img src="@/assets/Images/Isotipo.png" alt="Isotipo" width="30px"/> Petsuite
+        </b-navbar-brand>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <div>
+        <b-button href="/about" variant="light" class="ml-3"> Sign In</b-button>
+        <b-button href="/about" variant="light" class="ml-3"> Sign Up</b-button>
+        </div>
+      </b-navbar-nav>
+      </b-container>
+    </b-navbar>
     
     <router-view />
   </div>
 </template>
 
 <style>
+  .navbarPetSuite{
+    background: #40db9a;   
+  }
 </style>
