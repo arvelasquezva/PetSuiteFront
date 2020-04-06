d2ab0addc0c27d6478f2dc478ac076cf01bcf4be
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-04-05 22:48:27 -0500

    NavBarComponenteModificado

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,22 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="dark" class="navbarPetSuite">
-      <b-container>
-        <b-navbar-brand href="/">
-          <img src="@/assets/Images/Isotipo.png" alt="Isotipo" width="30px"/> Petsuite
-        </b-navbar-brand>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <div>
-        <b-button href="/login" variant="light" class="ml-3"> Sign In</b-button>
-        <b-button href="/about" variant="light" class="ml-3"> Sign Up</b-button>
-        </div>
-      </b-navbar-nav>
-      </b-container>
-    </b-navbar>
-    
+    <NavbarComponente></NavbarComponente>
     <router-view />
   </div>
 </template>
 
+<script>
+import NavbarComponente from "@/components/NavbarComponente.vue";
+export default {
+  components: {
+    NavbarComponente
+  }
+};
+</script>
 <style>
 #app {
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   place-items: center;
   background: #063869;
-  
-  }
-  .navbarPetSuite{
-    background: #40db9a;   
-  }
+}
 </style>
