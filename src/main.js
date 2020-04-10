9cb9397ac857277852236cd6a30bf9b61fd30512
JuanDiego98 <jlozanoc@unal.edu.co>
2020-04-10 02:24:05 -0500

    Added imports to main.js necessary for Axios use

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import router from './router';
 import store from './store';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
@@ -9,6 +11,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
