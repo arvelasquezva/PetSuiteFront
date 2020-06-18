3e9abac06c24048064446484a39c9fa01b6fcf93
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-06-17 21:26:47 -0500

    Adding Route for Deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@
 module.exports = {
     devServer: {
         //proxy: 'http://localhost:8080/'
-        proxy: 'http://186.29.15.219:9696/'
+        proxy: 'https://petsuite.herokuapp.com/'
     }
 }
