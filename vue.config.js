55737fd72768a43696e18b9cc9cb6d8cf0e1038d
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2020-06-18 11:49:08 -0500

    started working on tests

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js
 module.exports = {
     devServer: {
-        //proxy: 'http://localhost:8080/'
-        proxy: 'http://186.29.15.219:9696/'
+        proxy: 'http://localhost:8080/'
+       // proxy: 'http://186.29.15.219:9696/'
     }
 }
